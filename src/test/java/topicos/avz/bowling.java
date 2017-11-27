a14eec2cf91c1bada29939635cfee882011f82f8
Juan M A <juanma.bei94@gmail.com>
2017-11-27 16:32:19 -0200

    primer commit

---
+++ src/test/java/topicos/avz/bowling.java
@@ -0,0 +1,14 @@
+package topicos.avz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class bowling {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

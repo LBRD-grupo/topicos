7a29a04c82a9a94332462ff745eae4db8a677e52
Juan M A <juanma.bei94@gmail.com>
2017-11-27 11:26:24 -0200

    Comit Falla

---
+++ src/main/java/topicos/avz/calculadora.java
@@ -0,0 +1,10 @@
+package topicos.avz;
+
+public class calculadora {
+
+	public int suma(int a,int b) {
+		// TODO Auto-generated method stub
+		return a+b;
+	}
+
+}
---
+++ src/test/java/topicos/avz/pruebaTest.java
@@ -0,0 +1,16 @@
+package topicos.avz;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class pruebaTest {
+
+	@Test
+	public void test() {
+		calculadora var = new calculadora();
+		int result = var.suma(2,3);
+		assertEquals(6, result);
+	}
+
+}
